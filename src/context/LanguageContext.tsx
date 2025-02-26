ddaeddec721d12b8d025277a7a27b7f10d12a9ea
Tunahan <59062912+tkarakok@users.noreply.github.com>
2025-02-27 01:41:09 +0300

    a

--- src/context/LanguageContext.tsx
+++ src/context/LanguageContext.tsx
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useState, ReactNode } from 'react';
 
-type Language = 'tr' | 'en';
+type Language = 'tr';
 
 interface LanguageContextType {
   language: Language;
