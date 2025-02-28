c2d3b4e51bb39e59602aba4183d1cdac50ce6a23
Tunahan <59062912+tkarakok@users.noreply.github.com>
2025-02-28 04:05:33 +0300

    asd

--- src/components/Reservation.tsx
+++ src/components/Reservation.tsx
@@ -21,6 +21,12 @@ const Reservation: React.FC = () => {
     };
     return (
         <section id="gallery" className="py-20 bg-gray-50">
+            <div className="text-center mb-16">
+          <h2 className="text-3xl md:text-4xl font-bold text-primary mb-4">{t('reservation.title')}</h2>
+          <p className="text-gray-600 max-w-2xl mx-auto">
+            {t('resevation.subtitle')}
+          </p>
+        </div>
             <div className="reservation-form-container">
 
                 <form onSubmit={handleSubmit}>
