e2b8fd4ebb5a07aed5e21524f704b2d8b18fec45
Tunahan <59062912+tkarakok@users.noreply.github.com>
2025-02-28 04:12:03 +0300

    asd

--- src/components/Reservation.tsx
+++ src/components/Reservation.tsx
@@ -20,7 +20,7 @@ const Reservation: React.FC = () => {
         console.log('Form submitted');
     };
     return (
-        <section id="gallery" className="py-20 bg-gray-50">
+        <section id="gallery" className="py-10 bg-white-50">
             <div className="container mx-auto px-4">
                 <div className="text-center mb-16">
                     <h2 className="text-3xl md:text-4xl font-bold text-primary mb-4">{t('reservation.title')}</h2>
