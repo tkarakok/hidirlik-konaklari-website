818f356a608e72e815e743d70910e59c96c851d3
Tunahan <59062912+tkarakok@users.noreply.github.com>
2025-03-04 00:44:28 +0300

    bug fix

--- src/components/PanaromaViewer.tsx
+++ src/components/PanaromaViewer.tsx
@@ -1,35 +1,11 @@
-import React, { useState } from 'react';
-import { useLanguage } from '../context/LanguageContext';
-import { createRoot } from 'react-dom/client';
+import React, { useEffect } from 'react';
 import * as pannellum from 'https://unpkg.com/pannellum/build/pannellum.js';
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>360° Sanal Tur - Hıdırlık Konakları</title>
-  <link rel="stylesheet" href="https://unpkg.com/pannellum/build/pannellum.css">
-  <style>
-    #panorama {
-      width: 100%;
-      height: 100vh;
-    }
-  </style>
-</head>
-<body>
-  <div id="panorama"></div>
-
-  <div id="root"></div>
-  <script type="module" src="./panorama.tsx"></script>
-</body>
-</html>
 
 const PanoramaViewer: React.FC = () => {
   useEffect(() => {
     pannellum.viewer('panorama', {
       type: 'equirectangular',
-      panorama: 'path/to/your/360-image.jpg', // Buraya 360° görselinin yolunu ekle
+      panorama: 'path/to/your/360-image.jpg',
       autoLoad: true,
       showControls: true,
       compass: true,
@@ -40,11 +16,5 @@ const PanoramaViewer: React.FC = () => {
 
   return <></>;
 };
-
-const rootElement = document.getElementById('root');
-if (rootElement) {
-  const root = createRoot(rootElement);
-  root.render(<PanoramaViewer />);
-}
 
 export default PanoramaViewer;
