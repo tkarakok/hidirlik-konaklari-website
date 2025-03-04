d2bab5eabccd72c17b7e81c7ae92910984c14822
Tunahan <59062912+tkarakok@users.noreply.github.com>
2025-03-04 22:27:56 +0300

    asd

--- src/components/PanaromaViewer.tsx
+++ src/components/PanaromaViewer.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
+import { useLanguage } from '../context/LanguageContext';
 import { ChevronRight, ChevronLeft, Info, Home } from 'lucide-react';
 
 interface TourScene {
